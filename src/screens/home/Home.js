0fdecce0c049d0ecc95b39f84a6e291602138a91
root <nikhilbisht837@gmail.com>
2021-07-25 22:01:17 +0530

    Implement layout of the home page

---
+++ src/screens/home/Home.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+
+//importing the header component
+// import Header from '../../common/header/Header';
+
+//importing material-ui components
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+//importing the css file of the Home page
+import './Home.css';
+
+import { Redirect } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// Add all icons to the library so you can use it in your page
+library.add(fas, far, fab)
+
+const styles = theme => ({
+    restaurantsCard: {
+        width: 300,
+        maxWidth: 300,
+        height: 340,
+        maxHeight: 340,
+        marginTop: 15,
+        marginBottom: 10,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        paddingBottom: 15,
+        cursor: 'pointer',
+    }
+});
+
+//Home Section UI
+class Home extends Component {
+
+    // integrating search box with ui
+    searchHandler = (event) => {
+        let that = this;
+        let filteredRestaurants = null;
+        let xhrFilteredRestaurants = new XMLHttpRequest();
+        xhrFilteredRestaurants.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                if (!JSON.parse(this.responseText).restaurants) {
+                    that.setState({
+                        restaurants: null
+                    });
+                } else {
+                    that.setState({
+                        restaurants: JSON.parse(this.responseText).restaurants
+                    });
+                }
+            }
+        });
+        if (event.target.value === '') {
+            this.getRestaurants();
+        } else {
+            let url = this.props.baseUrl + 'restaurant/name/' + event.target.value;
+            xhrFilteredRestaurants.open("GET", url);
+            xhrFilteredRestaurants.send(filteredRestaurants);
+        }
+    }
+
+    // redirects to restaurant details page with restauranat id
+    restaurantDetails = (restaurantId) => {
+        this.props.history.push('/restaurant/' + restaurantId);
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            this.state.loggedIn === false ? <Redirect to="/" /> :
+                this.mounted === true ?
+                    <div>
+                        {/* <Header showSearchBox={true} searchHandler={this.searchHandler} baseUrl={this.props.baseUrl} /> */}
+                        {/* if no restaurants found with the entered name displays the No restaurant with the given name. */}
+                        {
+                            this.state.restaurants !== null ?
+
+                                this.state.restaurants.length === 0 && this.state.loading === false ?
+                                    <Typography variant="h6">No restaurant with the given name.</Typography> :
+                                    <ImageList cols={this.state.cards} rowHeight="auto">
+                                        {this.state.restaurants.map(restaurant => (
+                                            <ImageListItem key={'restaurant' + restaurant.id} >
+                                                {/* restaurant details card onclick redirects to restaurant details page*/}
+                                                <Card className={classes.restaurantsCard} onClick={() => this.restaurantDetails(restaurant.id)}>
+                                                    <CardActionArea>
+                                                        <CardMedia component="img" height={160} image={restaurant.photo_URL} title={restaurant.restaurant_name} />
+                                                        <CardContent>
+                                                            <div className="restaurant-title-div">
+                                                                <Typography gutterBottom variant='h5' component='h2'>
+                                                                    {restaurant.restaurant_name}
+                                                                </Typography>
+                                                            </div>
+                                                            <div className="restaurant-categories-div">
+                                                                <Typography variant='subtitle1'>
+                                                                    {restaurant.categories}
+                                                                </Typography>
+                                                            </div>
+                                                            <div className="rating-and-avg-div">
+                                                                {/* restaurant rating */}
+                                                                <div className="restaurant-rating-div">
+                                                                    <Typography variant='body1'>
+                                                                        <FontAwesomeIcon icon="star" /> {restaurant.customer_rating} ({restaurant.number_customers_rated})
+                                                                    </Typography>
+                                                                </div>
+                                                                {/* restaurant average price */}
+                                                                <div className="restaurant-avg-price-div">
+                                                                    <Typography variant='body1'>
+                                                                        <i className="fa fa-inr" aria-hidden="true"></i>
+                                                                        {restaurant.average_price} for two
+                                                                    </Typography>
+                                                                </div>
+                                                            </div>
+                                                        </CardContent>
+                                                    </CardActionArea>
+                                                </Card>
+                                            </ImageListItem>
+                                        ))}
+                                    </ImageList>
+                                :
+                                <Typography variant="h6">No restaurant with the given name.</Typography>
+                        }
+                    </div>
+                    : ""
+        )
+    }
+}
+
+export default withStyles(styles)(Home);
+
