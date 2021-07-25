243eeeb55ac0602a44c2fe7e10d1e767484063d2
Bhargava Raju <BhargavaR@anthology.com>
2021-07-26 01:20:41 +0530

    Adding assets to the projects

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Controller from './screens/Controller';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Controller from './screens/Controller.js';
 
-
-
-ReactDOM.render(<Controller/>,document.getElementById('root')
-
-// import { BrowserRouter as Router } from "react-router-dom";
-// import Controller from './screens/Controller';
-
-import Home from './screens/home/Home';
-
-ReactDOM.render(
-    <div>
-
-      <Home/>
-
-        <Controller/>
-
-    </div>,
-    document.getElementById('root')
-
-);
+ReactDOM.render(<Controller/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
