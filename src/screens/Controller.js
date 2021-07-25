3a6f43266aab533104806d2b71dfd987bc92c6f2
Bhargava Raju <BhargavaR@anthology.com>
2021-07-26 01:27:24 +0530

    Adding common utiities functions to the project

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "../screens/home/Home";
+import Checkout from "../screens/checkout/Checkout";
+import Details from "../screens/details/Details";
+import Profile from "../screens/profile/Profile";
 
-//Router import for redirection.
-import { Route, Switch } from "react-router-dom";
 
-//Imports of different pages in the application
-import Home from '../screens/home/Home';
-
-
-/**
- * This class represents the whole FoodOrdering Application Routing And Components.
- */
 class Controller extends Component {
-    constructor() {
+
+
+    constructor(){
         super();
-        this.baseUrl = 'http://localhost:8080/api/'
+        //will have to be replaced with the provided url
+        this.baseUrl= "http://localhost:8080/api/";
+    }
+    
+
+    render(){
+        return(
+            <Router>
+                <div className= "main-container">
+                    <Route exact path="/" render={props=> <Home {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path="/restaurant/:id" render={props=> <Details {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path="/checkout" render={props=> <Checkout {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path="/profile" render={props=> <Profile {...props} baseUrl={this.baseUrl}/>}/>                    
+                </div>
+            </Router>
+
+        );
     }
 
-    render() {
-        return (
-            <Switch>
-                <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
-            </Switch>
-        )
-    }
 }
 
 export default Controller;
