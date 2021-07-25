4b85ade118f7927053d6143f225486fd39875536
Bhargava Raju <BhargavaR@anthology.com>
2021-07-26 02:11:16 +0530

    Adding the event handler to the component

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -35,6 +35,149 @@ class Details extends Component {
       this.apiURL = "http://localhost:8080/api/";
   }
 
+  componentWillMount() {
+    //API call to get restaurant details by restaurant ID
+    let xhr_resDetails = new XMLHttpRequest();
+    let dataRes = null;
+    // Getting and splitting current url to get restaurant ID
+    let temp = this.props.location.pathname;
+    let resId = temp.split("/")[2];
+    let that = this;
+    xhr_resDetails.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+            const data = JSON.parse(this.responseText);
+            that.setState({
+              resData:data,
+              locality:data.address.locality,
+              city:data.address.city
+            });
+      }
+  });
+  xhr_resDetails.open("GET", this.apiURL+"restaurant/"+resId);
+  xhr_resDetails.setRequestHeader("Cache-Control", "no-cache");
+  xhr_resDetails.send(dataRes);
+  }
+
+//Add items to cart from the Category wise list
+addToCart = (item, category) => {
+    //Calling snack bar to display message
+    this.snackBarHandler("Item added to cart!");
+  const myCartItem = this.state.cartItems || { restaurant : this.state.resData, itemList: [], totalPrice: 0, totalItemCount: 0};
+  let findIndex = null;
+  //If the item is new, not already added into the list, then insert newly
+   let findItem = myCartItem.itemList.find((cartItem, index) => {
+       if(cartItem.item.id === item.id) {
+           findIndex = index;
+           return cartItem;
+       }
+       return undefined;
+   });
+   // If item already exists, only increment item quantiyt and price
+   if(findItem !== undefined){
+      findItem.quantity =  findItem.quantity + 1;
+      findItem.totalItemPrice = findItem.totalItemPrice + item.price;
+      myCartItem.itemList[findIndex] = findItem;
+      findIndex = null;
+      myCartItem.totalPrice = myCartItem.totalPrice + item.price;
+      myCartItem.totalItemCount = myCartItem.totalItemCount + 1;
+   } else {
+       // If not already added then creating temp object and doing other calculations
+      const cartItem = {
+          quantity : 1,
+          categoryName: category.category_name,
+          categoryId: category.id,
+          item: item,
+          totalItemPrice: item.price
+      }
+      myCartItem.totalPrice = myCartItem.totalPrice + item.price;
+      myCartItem.totalItemCount = myCartItem.totalItemCount + 1;
+      // Push items to cart
+      myCartItem.itemList.push(cartItem);
+  }       
+  
+  // Finally updating our myCartItem state 
+  this.setState({ cartItems: myCartItem});
+
+
+}
+
+// Removing item from cart
+    removeAnItemFromCart = (removeCartItem, index) => {
+      const myCartItem = this.state.cartItems;
+      // Match item based on index
+      let findItem = myCartItem.itemList[index];
+      // Update matched item based on index
+      findItem.quantity =  findItem.quantity - 1;
+      findItem.totalItemPrice = findItem.totalItemPrice - findItem.item.price;
+      myCartItem.totalPrice = myCartItem.totalPrice - findItem.item.price;
+      myCartItem.totalItemCount = myCartItem.totalItemCount - 1; 
+      
+      // Remove that item from cart - if the  quantity goes to or less than 0
+      if( findItem.quantity <= 0)  {
+          myCartItem.itemList.splice(index, 1);
+          this.snackBarHandler("Item removed from cart!");
+      }else{
+          myCartItem.itemList[index] = findItem;
+          this.snackBarHandler("Item quantity decreased by 1!");
+      }
+      this.setState({ cartItems: myCartItem});  
+
+  }
+
+//Adding item from My Cart
+  addAnItemFromCart = (addCartItem, index) => {
+      this.snackBarHandler("Item quantity increased by 1!");
+      const myCartItem = this.state.cartItems;
+      let findItem = myCartItem.itemList[index];
+       if(findItem !== undefined){
+          findItem.quantity =  findItem.quantity + 1;
+          findItem.totalItemPrice = findItem.totalItemPrice + findItem.item.price;
+          myCartItem.totalPrice = myCartItem.totalPrice + findItem.item.price;
+          myCartItem.totalItemCount = myCartItem.totalItemCount + 1;
+       }     
+       myCartItem.itemList[index] = findItem;
+      this.setState({ cartItems: myCartItem});    
+  }
+
+  
+    //Logout action from drop down menu on profile icon
+    loginredirect = () => {
+      sessionStorage.clear();
+      this.props.history.push({
+        pathname: "/"
+      });
+  }
+
+//SnackBar handler both open and close function
+snackBarHandler = (message) => {
+ this.setState({ snackBarOpen: false});
+  this.setState({ snackBarMessage: message});
+  this.setState({ snackBarOpen: true});
+}
+
+//Checkout button
+//Passess the selected items and restaurant details to Checkout page
+checkOutCart = (e) => {
+  const myCartItem = this.state.cartItems;
+  if( myCartItem.itemList.length <=0 ){
+      this.snackBarHandler("Please add an item to your cart!");
+      return;
+  }else {
+      if(sessionStorage.getItem("access-token") === null){
+          this.snackBarHandler("Please login first!");
+          return;
+      }else{
+          sessionStorage.setItem("restaurantDetails",JSON.stringify(this.state.resData));
+          //Redirecting to Checkout page
+          this.props.history.push({
+              pathname: "/checkout",
+              state: { chcartItems: this.state.cartItems,
+                totalCartItemsValue:this.state.cartItems.totalPrice }
+            })   
+      }
+  }
+}
+
   render(){
     return(<div className="mainDiv">
     
